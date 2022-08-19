f78b59d53f97b20fbb885b03e33cda01d0ff25ce
Grigory Tenitsky <uram17@mail.ru>
2022-08-19 21:21:02 +0300

    add ReadAndPrint3Integers

---
+++ Hackerrank/Easy/ReadAndPrint3Integers.java
@@ -0,0 +1,23 @@
+package Leetcode_and_Hackerrank.Hackerrank.Easy;
+
+import java.util.Scanner;
+
+/**
+ * Task
+ * In this challenge, you must read  integers from stdin and then print them to stdout.
+ * Each integer must be printed on a new line.
+ */
+
+public class ReadAndPrint3Integers {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
