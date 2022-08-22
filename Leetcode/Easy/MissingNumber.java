6d4a09152a70a792a9984e666db3a35f0ecb9334
Grigory Tenitsky <uram17@mail.ru>
2022-08-22 22:26:48 +0300

    add MissingNumber for LeetCode.Easy

---
+++ Leetcode/Easy/MissingNumber.java
@@ -0,0 +1,17 @@
+package Leetcode_and_Hackerrank.Leetcode.Easy;
+
+/**
+ * Given an array nums containing n distinct numbers in the range [0, n],
+ * return the only number in the range that is missing from the array.
+ */
+
+public class MissingNumber {
+    public int missingNumber(int[] nums) {
+        int sum = 0;
+        int expected = (1 + nums.length) * nums.length / 2;
+        for (int num : nums) {
+            sum += num;
+        }
+        return expected - sum;
+    }
+}
