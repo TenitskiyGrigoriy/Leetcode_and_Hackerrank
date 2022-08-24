67f1f3370d8316e67e9635249b52db56dbee9bef
Grigory Tenitsky <uram17@mail.ru>
2022-08-24 23:07:34 +0300

    add FindAllNumbersDisappearedInAnArray for LeetCode.Easy

---
+++ Leetcode/Easy/FindAllNumbersDisappearedInAnArray.java
@@ -0,0 +1,27 @@
+package Leetcode_and_Hackerrank.Leetcode.Easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given an array nums of n integers where nums[i] is in the range [1, n],
+ * return an array of all the integers in the range [1, n] that do not appear in nums.
+ */
+
+public class FindAllNumbersDisappearedInAnArray {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            int ret = Math.abs(nums[i]) - 1;
+            if (nums[ret] > 0) {
+                nums[ret] = -nums[ret];
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                list.add(i + 1);
+            }
+        }
+        return list;
+    }
+}
