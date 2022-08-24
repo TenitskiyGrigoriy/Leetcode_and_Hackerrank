36c7b6ef68cfe570a9d2503548d4656d94083dd0
Grigory Tenitsky <uram17@mail.ru>
2022-08-24 23:18:42 +0300

    add FindAllNumbersDisappearedInAnArray for LeetCode.Easy

--- Leetcode/Easy/FindAllNumbersDisappearedInAnArray.java
+++ Leetcode/Easy/FindAllNumbersDisappearedInAnArray.java
@@ -17,9 +17,9 @@ public class FindAllNumbersDisappearedInAnArray {
                 nums[ret] = -nums[ret];
             }
         }
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0) {
-                list.add(i + 1);
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] > 0) {
+                list.add(j + 1);
             }
         }
         return list;
