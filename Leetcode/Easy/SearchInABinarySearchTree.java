bf33328b359f5806186711669aed8ae40a7b9959
Grigory Tenitsky <uram17@mail.ru>
2022-09-13 21:41:46 +0300

    add SearchInABinarySearchTree for LeetCode.Easy

---
+++ Leetcode/Easy/SearchInABinarySearchTree.java
@@ -0,0 +1,42 @@
+package Leetcode_and_Hackerrank.Leetcode.Easy;
+
+/**
+ * You are given the root of a binary search tree (BST) and an integer val.
+ *
+ * Find the node in the BST that the node's value equals val and return the subtree rooted with that node.
+ * If such a node does not exist, return null.
+ */
+
+public class SearchInABinarySearchTree {
+    public TreeNode1 searchBST(TreeNode1 root, int val) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val == val) {
+            return root;
+        }
+
+        if (val < root.val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+}
+
+
+
+// Definition for a binary tree node.
+class TreeNode1 {
+    int val;
+    TreeNode1 left;
+    TreeNode1 right;
+    TreeNode1() {}
+    TreeNode1(int val) { this.val = val; }
+    TreeNode1(int val, TreeNode1 left, TreeNode1 right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
