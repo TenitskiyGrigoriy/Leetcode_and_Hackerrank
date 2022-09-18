4658d12dec88325690ca72312a27d54e2579031c
Grigory Tenitsky <uram17@mail.ru>
2022-09-18 13:04:21 +0300

    add ValidParentheses for LeetCode.Easy

---
+++ Leetcode/Easy/ValidParentheses.java
@@ -0,0 +1,43 @@
+package Leetcode_and_Hackerrank.Leetcode.Easy;
+
+import java.util.Stack;
+
+/**
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ *
+ * An input string is valid if:
+ *
+ * 1. Open brackets must be closed by the same type of brackets.
+ * 2. Open brackets must be closed in the correct order.
+ * 3. Every close bracket has a corresponding open bracket of the same type.
+ */
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+
+        Stack<Character> stack = new Stack<>();
+        for (char c: s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char leftBracket = stack.pop();
+                if (!isPair(leftBracket, c)) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    private boolean isPair(char left, char right) {
+        return (left == '(' && right == ')') ||
+               (left == '[' && right == ']') ||
+               (left == '{' && right == '}');
+    }
+}
