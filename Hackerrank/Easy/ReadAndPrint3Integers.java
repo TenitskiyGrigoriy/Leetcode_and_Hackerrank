ed0a6afbd9e9d414f29d74b03457cb62af0b8ea4
Grigory Tenitsky <uram17@mail.ru>
2023-03-31 23:06:29 +0400

    add ReadAndPrint3Integers

--- Hackerrank/Easy/ReadAndPrint3Integers.java
+++ Hackerrank/Easy/ReadAndPrint3Integers.java
@@ -15,7 +15,6 @@ public class ReadAndPrint3Integers {
         int a = scan.nextInt();
         int b = scan.nextInt();
         int c = scan.nextInt();
-
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
