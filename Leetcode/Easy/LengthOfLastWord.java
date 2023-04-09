22c450a03e847fece540d9556f7dd216da1254cf
Grigory Tenitsky <uram17@mail.ru>
2023-04-09 23:19:22 +0400

    add PlusOne

--- Leetcode/Easy/LengthOfLastWord.java
+++ Leetcode/Easy/LengthOfLastWord.java
@@ -3,9 +3,7 @@ package Leetcode_and_Hackerrank.Leetcode.Easy;
 /**
  * Given a string s consisting of words and spaces, return the length of the last word in the string.
  *
- * A word is a maximal
- * substring
- *  consisting of non-space characters only.
+ * A word is a maximal substring consisting of non-space characters only.
  */
 public class LengthOfLastWord {
     public int lengthOfLastWord(String s) {
