67eff601fde5f2968a6948b6699e436c14c0762f
Grigory Tenitsky <uram17@mail.ru>
2023-04-16 22:27:21 +0400

    add MergeSortedArray

---
+++ Leetcode/Easy/MergeSortedArray.java
@@ -0,0 +1,33 @@
+package Leetcode_and_Hackerrank.Leetcode.Easy;
+
+/**
+ * You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n,
+ * representing the number of elements in nums1 and nums2 respectively.
+ *
+ * Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+ *
+ * The final sorted array should not be returned by the function, but instead be stored inside the array nums1.
+ * To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged,
+ * and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+ */
+public class MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int k = 0;
+        for (int i = nums1.length - n; i < nums1.length; i++) {
+            nums1[i] = nums2[k];
+            k++;
+        }
+        boolean isSorted = true;
+        while (isSorted) {
+            isSorted = false;
+            for (int i = 1; i < nums1.length; i++) {
+                if (nums1[i - 1] > nums1[i]) {
+                    int temp = nums1[i - 1];
+                    nums1[i - 1] = nums1[i];
+                    nums1[i] = temp;
+                    isSorted = true;
+                }
+            }
+        }
+    }
+}
