b7153bc59a5178573f117f255be4849ea52492be
Grigory Tenitsky <uram17@mail.ru>
2023-04-25 23:05:44 +0400

    add MaximumSubarray

--- Leetcode/Medium/MaximumSubarray.java
+++ Leetcode/Medium/MaximumSubarray.java
@@ -11,7 +11,6 @@ public class MaximumSubarray {
     public int maxSubArray(int[] nums) {
         int sum = 0;
         int maxSum = Integer.MIN_VALUE;
-
         for (int num: nums) {
             sum = Math.max(sum, 0) + num;
             maxSum = Math.max(maxSum, sum);
