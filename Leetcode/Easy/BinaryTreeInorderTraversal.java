985e003edb16fb5655cbe702e2fe7eea70c2ed7a
Grigory Tenitsky <uram17@mail.ru>
2023-05-14 23:18:24 +0400

    add BinaryTreeInorderTraversal

---
+++ Leetcode/Easy/BinaryTreeInorderTraversal.java
@@ -0,0 +1,45 @@
+package Leetcode_and_Hackerrank.Leetcode.Easy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Given the root of a binary tree, return the inorder traversal of its nodes' values.
+ */
+public class BinaryTreeInorderTraversal {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+        while (current != null || !stack.isEmpty()) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+            current = stack.pop();
+            result.add(current.val);
+            current = current.right;
+        }
+        return result;
+    }
+}
