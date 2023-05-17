347623c03dfc3028c8c46efc819b9aee1f73007e
Grigory Tenitsky <uram17@mail.ru>
2023-05-18 00:01:20 +0400

    add SumOfNodesWithEvenValuedGrandparent

--- Leetcode/Medium/SumOfNodesWithEvenValuedGrandparent.java
+++ Leetcode/Medium/SumOfNodesWithEvenValuedGrandparent.java
@@ -2,28 +2,33 @@ package Leetcode_and_Hackerrank.Leetcode.Medium;
 
 /**
  * You are given two binary trees root1 and root2.
- *
+ * <p>
  * Imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not.
  * You need to merge the two trees into a new binary tree. The merge rule is that if two nodes overlap,
  * then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of the new tree.
- *
+ * <p>
  * Return the merged tree.
- *
+ * <p>
  * Note: The merging process must start from the root nodes of both trees.
  */
 public class SumOfNodesWithEvenValuedGrandparent {
-  * public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
 
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
